3ea3dffcd1ceacf221dbc317342014e58aea4cb5
green224 <green224@users.noreply.github.com>
2022-07-16 22:39:13 +0900

    周りの色を吸わないギリギリのUV島を生成した際のIslandプレビューを行う機能を追加

---
+++ addons/IzTools2/opSet_uv_island_preview.py
@@ -0,0 +1,294 @@
+
+#
+# ミップマップを考慮してバイリニアの隣接ピクセルの色吸い問題が起きないように
+# エッジ拡張をすると、どれだけの広さになるかを可視化するモジュール。
+#
+
+
+import bpy
+import bmesh
+from mathutils import *
+import math
+
+from bpy.types import (
+		Operator,
+		Panel,
+		PropertyGroup,
+		OperatorFileListElement,
+		)
+
+from bpy.props import (
+		BoolProperty,
+		PointerProperty,
+		StringProperty, 
+		CollectionProperty,
+		FloatProperty,
+		EnumProperty,
+		IntProperty,
+		)
+
+import gpu
+import bgl
+from gpu_extras.batch import batch_for_shader
+from gpu_extras.presets import draw_circle_2d
+
+
+from .opSet_base import *
+from .common_uv import *
+
+
+#-------------------------------------------------------
+
+# 機能本体
+class OperatorSet(OperatorSet_Base):
+
+	# オペレータ本体
+	class OpImpl(Operator):
+		bl_idname = "object.izt_island_preview_uv"
+		bl_label = "Iz Tools: UV: Island Preview"
+
+		def execute(self, context):
+			cls = OperatorSet.OpImpl
+			if context.area.type == 'IMAGE_EDITOR':
+
+				if cls.is_running():
+					# 描画関数の登録を解除
+					bpy.types.SpaceImageEditor.draw_handler_remove(
+						cls.__hdl_drawOffsc, 'WINDOW' )
+					bpy.types.SpaceImageEditor.draw_handler_remove(
+						cls.__hdl_drawImgEdt, 'WINDOW' )
+					cls.__hdl_drawOffsc = None
+					cls.__hdl_drawImgEdt = None
+				else:
+					# 描画関数の登録
+					cls.__hdl_drawOffsc = bpy.types.SpaceImageEditor.draw_handler_add(
+						cls.__drawOffSc, (context, ), 'WINDOW', 'PRE_VIEW' )
+					cls.__hdl_drawImgEdt = bpy.types.SpaceImageEditor.draw_handler_add(
+						cls.__drawImgEdt, (context, ), 'WINDOW', 'POST_VIEW' )
+
+				if context.area:
+					context.area.tag_redraw()
+				return {'FINISHED'}
+			else:
+				return {'CANCELLED'}
+		
+		# 描画中か否か
+		@classmethod
+		def is_running(cls):
+			return True if cls.__hdl_drawImgEdt else False
+
+		# 描画ハンドラ
+		__hdl_drawOffsc = None
+		__hdl_drawImgEdt = None
+
+		# 作業用オフスクリーンバッファ
+		__offscreen0 = None
+		__offscreen1 = None
+		
+		# シェーダおよび描画用バッチ
+		__shader_Offsc = None
+		__shader_ImgEdt = None
+		__batch_ImgEdt = None
+
+		@classmethod
+		def __drawOffSc(cls, context):
+			# シェーダおよび描画用バッチの初期化
+			if cls.__shader_Offsc is None:
+				vertex_shader = '''
+					in vec2 uv;
+					uniform float texSize;
+					uniform vec2 pixelOffset;
+
+					void main()
+					{
+						vec2 pos = uv + pixelOffset / texSize;
+						gl_Position = vec4(pos*2-1, 0.0, 1.0);
+					}
+				'''
+				fragment_shader = '''
+					uniform vec4 color;
+
+					out vec4 FragColor;
+
+					void main()
+					{
+						FragColor = color;
+					}
+				'''
+				cls.__shader_Offsc = gpu.types.GPUShader(vertex_shader, fragment_shader)
+
+			# レンダリング先のテクスチャサイズ
+			param = context.scene.iz_uv_tool_property
+			texSize = 2 ** param.island_preview_reso_level
+
+			# 作業用オフスクリーンバッファの作成
+			if cls.__offscreen0 is None or cls.__offscreen0.width != texSize:
+				if cls.__offscreen0 is not None:
+					cls.__offscreen0.free()
+					cls.__offscreen1.free()
+				cls.__offscreen0 = gpu.types.GPUOffScreen(texSize, texSize)
+				cls.__offscreen1 = gpu.types.GPUOffScreen(texSize, texSize)
+			
+			# バッチを作成
+			bmList = getEditingBMeshList()
+			bMeshes = [bm for _,bm in bmList]
+			uvs = []
+			indices_tri = []
+			indices_line = []
+			for bm in bMeshes:
+				bm = bm.copy()
+				uv_layer = bm.loops.layers.uv.active
+				for face in bm.faces:
+					i0 = len(uvs)
+#					if face.lolen(ops) < 3: break	これは無くても一応大丈夫
+
+					tris = [0,0,0]
+					for loop in face.loops:
+						uv = loop[uv_layer]
+
+						i1 = len(uvs) - i0
+						if i1 < 2:		# Triangleのシーケンスを格納
+							tris[i1] = len(uvs)
+						elif i1 == 2:
+							tris[2] = len(uvs)
+							indices_tri.append( tris )
+						else:
+							indices_tri.append( [i0, len(uvs)-1, len(uvs)] )
+						if i1 != 0:		# Lineのシーケンスを格納
+							indices_line.append( [len(uvs)-1, len(uvs)] )
+
+						uvs.append( [ uv.uv.x, uv.uv.y ] )
+#						if uv.select: result.append( (uv, loop.vert) )
+					if len(uvs)-1 != i0:		# Lineのシーケンスが一つ足りてないので、最後の一つを格納
+						indices_line.append( [len(uvs)-1, i0] )
+				bm.free()
+			batch_Tri = batch_for_shader(cls.__shader_Offsc, 'TRIS', {"uv": uvs}, indices=indices_tri)
+			batch_Line = batch_for_shader(cls.__shader_Offsc, 'LINES', {"uv": uvs}, indices=indices_line)
+
+			# レンダリング先のテクスチャサイズ
+			param = context.scene.iz_uv_tool_property
+			texSize = 2 ** param.island_preview_reso_level
+
+			# オフスクリーンバッファへ描画
+			ofsSize = 0.49
+			pixelOffsets = [(-ofsSize,-ofsSize),(0,-ofsSize),(ofsSize,-ofsSize),(-ofsSize,0),(ofsSize,0),(-ofsSize,ofsSize),(0,ofsSize),(ofsSize,ofsSize)]
+			with cls.__offscreen0.bind():
+				fb = gpu.state.active_framebuffer_get()
+				fb.clear(color=(0.0, 0.0, 0.0, 1.0), depth=1)
+
+				gpu.state.face_culling_set("NONE")
+				cls.__shader_Offsc.bind()
+				cls.__shader_Offsc.uniform_float("texSize", texSize)
+				cls.__shader_Offsc.uniform_float("color", (1,0,0,1))
+				cls.__shader_Offsc.uniform_float("pixelOffset", (0,0))
+				batch_Tri.draw(cls.__shader_Offsc)
+				for pixelOfs in pixelOffsets:
+					cls.__shader_Offsc.uniform_float("pixelOffset", pixelOfs)
+					batch_Line.draw(cls.__shader_Offsc)
+
+
+		@classmethod
+		def __drawImgEdt(cls, context):
+			# シェーダおよび描画用バッチの初期化
+			if cls.__shader_ImgEdt is None:
+				vertex_shader = '''
+					// ModelViewProjectionMatrix : source/blender/gpu/shaders/gpu_shader_2D_vert.glsl
+					uniform mat4 ModelViewProjectionMatrix;
+
+					in vec2 position;
+					in vec2 uv;
+
+					out vec2 uvInterp;
+
+					void main()
+					{
+						uvInterp = uv;
+						gl_Position = ModelViewProjectionMatrix * vec4(position, 0.0, 1.0);
+					}
+				'''
+				fragment_shader = '''
+					uniform sampler2D image;
+					uniform float texSize;
+
+					in vec2 uvInterp;
+					out vec4 FragColor;
+
+					void main()
+					{
+						// gpuモジュールにはフィルタリングの設定がないので
+						// シェーダ側でポイントフィルタのように取得するように加工する
+						FragColor = textureLod( image, (floor(uvInterp*texSize)+0.5)/texSize, 0 );
+					}
+				'''
+				cls.__shader_ImgEdt = gpu.types.GPUShader(vertex_shader, fragment_shader)
+				cls.__batch_ImgEdt = batch_for_shader(
+					cls.__shader_ImgEdt, 'TRI_FAN',
+					{
+						"position": ((0, 0), (1, 0), (1, 1), (0, 1)),
+						"uv": ((0, 0), (1, 0), (1, 1), (0, 1)),
+					},
+				)
+
+			# レンダリング先のテクスチャサイズ
+			param = context.scene.iz_uv_tool_property
+			texSize = 2 ** param.island_preview_reso_level
+
+			fb = gpu.state.active_framebuffer_get()
+			gpu.state.blend_set("ADDITIVE")
+
+			cls.__shader_ImgEdt.bind()
+			cls.__shader_ImgEdt.uniform_sampler("image", cls.__offscreen0.texture_color)
+# これが上手く適応できないので、とりあえずシェーダ側でむりやりポイントフィルタにする
+#			bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_NEAREST)
+#			bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_NEAREST)
+			cls.__shader_ImgEdt.uniform_float("texSize", texSize)
+			cls.__batch_ImgEdt.draw(cls.__shader_ImgEdt)
+
+	# UIパネル描画部分
+	class UI_PT_Izt_UV_Island_Preview(OperatorSet_Base.Panel_Base):
+		bl_space_type = "IMAGE_EDITOR"
+		bl_region_type = "UI"
+		header_name = "Island Preview"
+
+		def draw(self, context):
+			layout = self.layout
+			
+			column = layout.column()
+
+			row = column.row()
+			param = context.scene.iz_uv_tool_property
+			resoLevel = param.island_preview_reso_level
+			row.label(text="resolution: " + str(2**resoLevel))
+
+			row = column.row()
+			row.prop(param, "island_preview_reso_level", slider=True)
+
+			column.separator()
+
+			row = column.row()
+			if OperatorSet.OpImpl.is_running():
+				row.operator(OperatorSet.OpImpl.bl_idname, text="Hide", icon="PAUSE")
+			else:
+				row.operator(OperatorSet.OpImpl.bl_idname, text="Show", icon="PLAY")
+
+	def __init__(self, props):
+		super().__init__()
+
+		# 登録対象のクラスリストを定義
+		self._classes = (
+			OperatorSet.UI_PT_Izt_UV_Island_Preview,
+			OperatorSet.OpImpl,
+		)
+
+		# Global保存パラメータを定義
+		a = IntProperty(
+			name="reso level",
+			description="Texture Resolution Level",
+			default=9,
+			min=4,
+			max=12,
+		)
+		props.island_preview_reso_level: a
+		props.__annotations__["island_preview_reso_level"] = a
+
+
